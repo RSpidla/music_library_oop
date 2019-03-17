9897db2bd884bba49723816b4ad3f3eab7296d37
‘RSpidla’ <rspidla@gmail.com>
2019-03-17 19:16:50 +0000

    Add Library with multiple playlists (starts as an empty array)

--- music_library.js
+++ music_library.js
@@ -2,6 +2,7 @@
 // Library constructor function declaration
 // Library has 'name' as string
 // Library has 'creator' as string
+// Library has 'playlists' as empty array
 function Library(name, creator) {
   this.name = name;
   this.creator = creator;
