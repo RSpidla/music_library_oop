d2129da9f0f77fb982b5a873b8da201cdf27d473
‘RSpidla’ <rspidla@gmail.com>
2019-03-17 19:12:50 +0000

    Add Library with name and a creator both strings

--- music_library.js
+++ music_library.js
@@ -1,5 +1,5 @@
 
-// Library function declaration
+// Library constructor function declaration
 // Library has 'name' as string
 // Library has 'creator' as string
 function Library(name, creator) {
